0c12623fc80361e053fccd25f660f81efc6878bf
Danial A <danazad2016@gmail.com>
2021-05-08 17:54:44 -0500

    Added More conditions

--- formValidation.js
+++ formValidation.js
@@ -12,13 +12,54 @@ const verification = () => {
   if (usernameInput.value.length < 3) {
     usernameInput.setAttribute('class', 'error')
     messageDiv.textContent = `Your username can't have less then 3 characters`
+  }
+
+  if (usernameInput.value.length >= 3 && usernameInput.value.length <= 20) {
+    usernameInput.removeAttribute('class', 'error')
+    messageDiv.textContent = ``
+  }
+
+  if (passwordVerificationInput.value.length === 0) {
+    passwordVerificationInput.setAttribute('class', 'error')
+    messageDiv.textContent = "Please retype your password in Password Verifier"
+  }
+
+  if (usernameInput.value.length === 0) {
+    usernameInput.setAttribute('class', 'error')
+    messageDiv.textContent = `You need to provide a username please`
+  }
+
+  if (passwordInput.value.length > 20) {
+    passwordInput.setAttribute('class', 'error')
+    messageDiv.textContent = `Your password can't have more then 20 characters`
   } 
 
-  if (usernameInput.value.length > 3 && usernameInput.value.length < 20) {
-    usernameInput.removeAttribute('class', 'error')
-    usernameInput.setAttribute('class', 'neutral')
-    messageDiv.textContent = ""
+  if (passwordInput.value.length < 5) {
+    passwordInput.setAttribute('class', 'error')
+    messageDiv.textContent = `Your password can't have less then 5 characters`
+  } 
+
+  if (passwordInput.value.length >= 5 && passwordInput.value.length <= 20) {
+    passwordInput.removeAttribute('class', 'error')
+    messageDiv.textContent = ``
   }
+
+  if (passwordInput.value !== passwordVerificationInput.value) {
+    passwordInput.setAttribute('class', 'error')
+    passwordVerificationInput.setAttribute('class', 'error')
+    messageDiv.textContent = "Your password in Password and Password Verifier don't match"
+  }
+
+  if (passwordInput.value === passwordVerificationInput.value) {
+    passwordVerificationInput.removeAttribute('class', 'error')
+  }
+
+  if (passwordInput.value.length === 0) {
+    passwordInput.setAttribute('class', 'error')
+    messageDiv.textContent = "You need to provide a password for your account please"
+
+  }
+
 }
 
 document.addEventListener('click', event => {
